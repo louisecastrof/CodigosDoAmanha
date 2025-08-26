2e23663cdb669a79cfefc89ee3f4d88519b2d007
louisecastrof <louise.castro@grupoboticario.com.br>
2025-08-25 21:49:59 -0300

    :sparkles: feat add espaço

--- Módulo 3 - Aula 4/codigo_math.py
+++ Módulo 3 - Aula 4/codigo_math.py
@@ -5,6 +5,6 @@ import math
 raiz_quadrada = math.sqrt(16)
 print(raiz_quadrada)  # Saída: 4.0
 
-# Calcula o valor de pi
+# Calcula o valor de pi 
 valor_pi = math.pi
 print(valor_pi)  # Saída: 3.141592653589793
